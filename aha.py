0b6460abc846ef86185a00f3dfc8c7e5ea217a76
Anh-Thang <thangnguyenanh168@gmail.com>
2023-12-06 18:50:19 +0700

    first commit

---
+++ aha.py
@@ -0,0 +1,16 @@
+print("Hello KHOA!")
+name= input("What is your name? ")
+diem= float(input("diemtb: "))
+if diem>=9.0:
+    print("A")
+elif diem>=7.0:
+    print("B")
+elif diem>=5:
+    print("C")
+else:
+    print("F")
+
+
+
+
+
